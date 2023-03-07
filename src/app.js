0a40f7c2a2a242a04f0ee84fa1cbbc95bbae9120
oskarlaptop <oskar.lr.sterner@gmail.com>
2023-03-07 08:07:22 +0100

    helmet configuration to allow images

--- src/app.js
+++ src/app.js
@@ -11,7 +11,14 @@ const { sequelize } = require("./database/config");
 const app = express();
 
 app.use(express.json());
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"]
+    }
+  })
+);
 app.use(cors());
 
 app.use((req, res, next) => {
